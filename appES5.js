4fb9ce31034b43fb93495146557bda84c4ada1aa
Ahmed Gürkan Özçelik <ahmetgozcelik@gmail.com>
2022-09-01 10:20:01 +0300

    The course object was created.

---
+++ appES5.js
@@ -0,0 +1,21 @@
+function Course(title,instructor,image){
+    this.title = title;
+    this.instructor = instructor;
+    this.image = image;
+}
+
+
+
+document.getElementById('new-course').addEventListener('submit', function(e){
+
+    const title = document.getElementById('title').value;
+    const instructor = document.getElementById('instructor').value;
+    const image = document.getElementById('image').value;
+
+    // create course object
+    const course = new Course(title,instructor,image);
+
+    
+
+    e.preventDefault();
+});
